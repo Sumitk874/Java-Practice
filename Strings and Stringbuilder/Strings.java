61ab1968d41fa83d502fb801b6d10b62714ae3df
Paramjeet Kumar <69776082+Sumitk874@users.noreply.github.com>
2023-07-16 05:02:51 +0000

    initial commit

---
+++ Strings and Stringbuilder/Strings.java
@@ -0,0 +1,52 @@
+public class Strings{
+    public static void main(String[] args) {
+        String m = "abccbc";
+        String n = "abccbc";
+        
+        
+        
+        // Interning
+        // m and n point to the same string 
+        // if a string of same value is present inside intern pool then new variable gets the address of that pre existing string to point at , this is called interning(Sharing memory by string to optimise space)
+        // to avoid interning we can use 'new' keyword
+        
+        // Comparison
+        // don't use == instead use .equals()
+        // == checks for address they're pointing 
+
+
+
+        subString(m);
+        // System.out.println(isPalindrome(m));
+    }
+
+    static void subString(String s){
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i+1; j <= s.length(); j++) {
+                // System.out.println(s.substring(i, j));
+                String m = s.substring(i, j);
+                if(isPalindrome(m)==true) System.out.println(m); // to print all palindromic substrings
+            }
+        }
+    }
+
+
+    static boolean isPalindrome(String s){
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while (left <= right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false; // Characters at left and right positions don't match
+            }else{
+                left++;
+                right--;
+            }
+            
+        }
+        
+        return true; 
+    }
+}
+
+
